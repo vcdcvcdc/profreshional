6ff5666e7f4beeb906e48f324463dda36edce420
vcdcvcdc <86494090+vcdcvcdc@users.noreply.github.com>
2023-11-09 17:44:40 -0800

    Update server.js

--- server.js
+++ server.js
@@ -2,10 +2,12 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'public')));
+// Serve static files from the root directory
+app.use(express.static(__dirname));
 
+// Send 'webcam2.html' for any other requests
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'webcam2.html'));
+  res.sendFile(path.join(__dirname, 'webcam2.html'));
 });
 
 const PORT = process.env.PORT || 3000;
